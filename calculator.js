c71de27dc8eb1c7c2d7245a1c849200f8eab761a
Ben Fullenkamp <168487288+bfull8@users.noreply.github.com>
2026-08-22 22:16:11 -0400

    create strings using button presses for num1, num2, and operator

--- calculator.js
+++ calculator.js
@@ -31,15 +31,37 @@ function operate(operator, a, b) {
   }
 }
 
-let num1;
-let num2;
-let operator;
+let num1 = "";
+let num2 = "";
+let operator = "";
 
+// Add an event listener to each button
 const buttons = document.querySelectorAll("button");
-
 buttons.forEach((button) =>
   button.addEventListener("click", (event) => {
     let value = event.target.value;
-    console.log(value);
+
+    if (value === "-" && num1 === "") {
+      num1 = "-";
+    } else if (value === "-" && operator !== "" && num2 === "") {
+      num2 = "-";
+    } else if (!"/+-*".includes(value)) {
+      if (operator === "") {
+        if (value === "." && num1.includes(".")) {
+          // do nothing
+        } else {
+          num1 += value;
+        }
+      } else {
+        if (value === "." && num2.includes(".")) {
+          // do nothing
+        } else {
+          num2 += value;
+        }
+      }
+    } else {
+      operator = value;
+    }
+    console.log(num1, operator, num2);
   }),
 );
